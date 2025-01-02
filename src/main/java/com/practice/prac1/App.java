7e56b4a9a5be21016f274dc127dd5819b8b1719f
saanvi <saanvibajjuri@gmail.com>
2025-01-02 19:49:56 +0530

    practice1 done

---
+++ src/main/java/com/practice/prac1/App.java
@@ -0,0 +1,13 @@
+package com.practice.prac1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello prac1!" );
+    }
+}
